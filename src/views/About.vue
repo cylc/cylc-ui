d561928c5c1ccee84609ba2999e06b6216c8fbf0
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-01-04 12:05:27 +1300

    Rename Cylc WEB GUI to just Cylc Web

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,7 @@
 <template>
   <div>
     <h1>Help</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>The following links </p>
     <div class="row">
       <div class="col-sm-3">
         <div class="card">
@@ -61,9 +58,5 @@
         </div>
       </div>
     </div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-      Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
   </div>
 </template>
