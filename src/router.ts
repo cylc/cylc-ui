ca5e29ef94eaaf0840386a047ce65ed1c8654cdf
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-01-04 10:47:32 +1300

    Remove comments

--- src/router.ts
+++ src/router.ts
@@ -16,10 +16,7 @@ export default new Router({
     {
       path: '/help',
       name: 'help',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: 'about' */ './views/Help.vue'),
+      component: () => import('./views/Help.vue'),
     },
   ],
 });
