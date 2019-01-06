389fa6384f19f8001c44d0c2651d4375615e25fd
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-01-07 11:23:50 +1300

    Remove drawer's background image

--- src/components/core/Drawer.vue
+++ src/components/core/Drawer.vue
@@ -9,56 +9,51 @@
     mobile-break-point="991"
     width="260"
   >
-    <v-img
-      :src="image"
-      height="100%"
+    <v-layout
+      class="fill-height"
+      tag="v-list"
+      column
     >
-      <v-layout
-        class="fill-height"
-        tag="v-list"
-        column
+      <v-list-tile avatar>
+        <v-list-tile-avatar
+          color="white"
+        >
+          <v-img
+            :src="logo"
+            height="34"
+            contain
+          />
+        </v-list-tile-avatar>
+        <v-list-tile-title class="title">
+          Cylc Web
+        </v-list-tile-title>
+      </v-list-tile>
+      <v-divider/>
+      <v-list-tile
+        v-if="responsive"
       >
-        <v-list-tile avatar>
-          <v-list-tile-avatar
-            color="white"
-          >
-            <v-img
-              :src="logo"
-              height="34"
-              contain
-            />
-          </v-list-tile-avatar>
-          <v-list-tile-title class="title">
-            Cylc Web
-          </v-list-tile-title>
-        </v-list-tile>
-        <v-divider/>
-        <v-list-tile
-          v-if="responsive"
-        >
-          <v-text-field
-            class="purple-input search-input"
-            label="Search..."
-            color="purple"
-          />
-        </v-list-tile>
-        <v-list-tile
-          v-for="(link, i) in links"
-          :key="i"
-          :to="link.to"
-          :active-class="color"
-          avatar
-          class="v-list-item"
-        >
-          <v-list-tile-action>
-            <v-icon>{{ link.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title
-            v-text="link.text"
-          />
-        </v-list-tile>
-      </v-layout>
-    </v-img>
+        <v-text-field
+          class="purple-input search-input"
+          label="Search..."
+          color="purple"
+        />
+      </v-list-tile>
+      <v-list-tile
+        v-for="(link, i) in links"
+        :key="i"
+        :to="link.to"
+        :active-class="color"
+        avatar
+        class="v-list-item"
+      >
+        <v-list-tile-action>
+          <v-icon>{{ link.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-title
+          v-text="link.text"
+        />
+      </v-list-tile>
+    </v-layout>
   </v-navigation-drawer>
 </template>
 
@@ -92,7 +87,7 @@ export default {
     responsive: false
   }),
   computed: {
-    ...mapState('app', ['image', 'color']),
+    ...mapState('app', ['color']),
     inputValue: {
       get () {
         return this.$store.state.app.drawer
