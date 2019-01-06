57db434fb5e743f2af84b66ff2181f6a13cc2974
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-01-07 11:32:29 +1300

    Fix eslint

--- src/views/Suites.vue
+++ src/views/Suites.vue
@@ -83,65 +83,65 @@
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      headers: [
-        {
-          sortable: false,
-          text: 'Name',
-          value: 'name'
-        },
-        {
-          sortable: false,
-          text: 'Country',
-          value: 'country'
-        },
-        {
-          sortable: false,
-          text: 'City',
-          value: 'city'
-        },
-        {
-          sortable: false,
-          text: 'Salary',
-          value: 'salary',
-          align: 'right'
-        }
-      ],
-      items: [
-        {
-          name: 'Dakota Rice',
-          country: 'Niger',
-          city: 'Oud-Tunrhout',
-          salary: '$35,738'
-        },
-        {
-          name: 'Minerva Hooper',
-          country: 'Curaçao',
-          city: 'Sinaai-Waas',
-          salary: '$23,738'
-        }, {
-          name: 'Sage Rodriguez',
-          country: 'Netherlands',
-          city: 'Overland Park',
-          salary: '$56,142'
-        }, {
-          name: 'Philip Chanley',
-          country: 'Korea, South',
-          city: 'Gloucester',
-          salary: '$38,735'
-        }, {
-          name: 'Doris Greene',
-          country: 'Malawi',
-          city: 'Feldkirchen in Kārnten',
-          salary: '$63,542'
-        }, {
-          name: 'Mason Porter',
-          country: 'Chile',
-          city: 'Gloucester',
-          salary: '$78,615'
-        }
-      ]
-    })
-  }
+export default {
+  data: () => ({
+    headers: [
+      {
+        sortable: false,
+        text: 'Name',
+        value: 'name'
+      },
+      {
+        sortable: false,
+        text: 'Country',
+        value: 'country'
+      },
+      {
+        sortable: false,
+        text: 'City',
+        value: 'city'
+      },
+      {
+        sortable: false,
+        text: 'Salary',
+        value: 'salary',
+        align: 'right'
+      }
+    ],
+    items: [
+      {
+        name: 'Dakota Rice',
+        country: 'Niger',
+        city: 'Oud-Tunrhout',
+        salary: '$35,738'
+      },
+      {
+        name: 'Minerva Hooper',
+        country: 'Curaçao',
+        city: 'Sinaai-Waas',
+        salary: '$23,738'
+      }, {
+        name: 'Sage Rodriguez',
+        country: 'Netherlands',
+        city: 'Overland Park',
+        salary: '$56,142'
+      }, {
+        name: 'Philip Chanley',
+        country: 'Korea, South',
+        city: 'Gloucester',
+        salary: '$38,735'
+      }, {
+        name: 'Doris Greene',
+        country: 'Malawi',
+        city: 'Feldkirchen in Kārnten',
+        salary: '$63,542'
+      }, {
+        name: 'Mason Porter',
+        country: 'Chile',
+        city: 'Gloucester',
+        salary: '$78,615'
+      }
+    ]
+  })
+}
 </script>
