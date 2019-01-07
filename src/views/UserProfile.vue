219f00a34318d6bb045f4f69ea34430fdbeaa583
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-01-07 14:51:48 +1300

    Display less fields on the user page

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -21,28 +21,24 @@
               <v-layout wrap>
                 <v-flex
                   xs12
-                  md4
-                >
-                  <v-text-field
-                    label="Company (disabled)"
-                    disabled/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md4
+                  md6
                 >
                   <v-text-field
                     class="purple-input"
                     label="User Name"
+                    value="robert.typer"
+                    disabled
                   />
                 </v-flex>
                 <v-flex
                   xs12
-                  md4
+                  md6
                 >
                   <v-text-field
                     label="Email Address"
-                    class="purple-input"/>
+                    class="purple-input"
+                    value="Robert.Typer@company.co.nz"
+                  />
                 </v-flex>
                 <v-flex
                   xs12
@@ -50,7 +46,9 @@
                 >
                   <v-text-field
                     label="First Name"
-                    class="purple-input"/>
+                    class="purple-input"
+                    value="Robert"
+                  />
                 </v-flex>
                 <v-flex
                   xs12
@@ -58,43 +56,8 @@
                 >
                   <v-text-field
                     label="Last Name"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md12
-                >
-                  <v-text-field
-                    label="Adress"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md4>
-                  <v-text-field
-                    label="City"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md4>
-                  <v-text-field
-                    label="Country"
-                    class="purple-input"/>
-                </v-flex>
-                <v-flex
-                  xs12
-                  md4>
-                  <v-text-field
                     class="purple-input"
-                    label="Postal Code"
-                    type="number"/>
-                </v-flex>
-                <v-flex xs12>
-                  <v-textarea
-                    class="purple-input"
-                    label="About Me"
-                    value="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+                    value="Typer"
                   />
                 </v-flex>
                 <v-flex
