4f41c5b8546cc61a52fba9c150c533b8ac67cc23
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-02-08 10:29:53 +1300

    Issue #12 move icons to vuetify plugin

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import theme from './theme'
 import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify, {
