e27e7379653773b25e4a5179455fcfda34e743d1
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-02-12 14:03:20 +1300

    Add optional vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// optional file, loaded automatically by @vue/cli-service if present next to package.json
+
+module.exports = {
+  publicPath: '/',
+  outputDir: 'dist',
+  indexPath: 'index.html',
+  configureWebpack: {
+    
+  }
+}
