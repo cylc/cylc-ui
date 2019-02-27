b6c1660d35ee21f5f1e64835023e15aa145fe5b2
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-02-28 09:41:39 +1300

    Issue #38 Fix unit test for User model

--- tests/unit/model/user.spec.js
+++ tests/unit/model/user.spec.js
@@ -6,16 +6,14 @@ import User from '@/model/User.model.js'
 describe('UserModel', () => {
   describe('constructor', () => {
     it('should be created', () => {
-      const data = {
-        name: 'john.foe',
-        groups: ['abc', 'wheel'],
-        admin: true,
-        created: 'now'
-      }
-      const user = new User(data)
-      expect(user.getUserName()).to.equal('john.foe')
-      expect(user.getGroups().length === 2).to.equal(true)
-      expect(user.isAdmin()).to.equal(true)
+      const name = 'john.foe';
+      const groups = ['abc', 'wheel'];
+      const admin = true;
+      const created = 'now';
+      const user = new User(name, groups, created, admin);
+      expect(user.getUserName()).to.equal('john.foe');
+      expect(user.getGroups().length === 2).to.equal(true);
+      expect(user.isAdmin()).to.equal(true);
       expect(user.getCreated()).to.equal('now')
     })
   })
