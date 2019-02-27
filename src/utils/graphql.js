2a35505bdbf3ee3313cae82a385d24ae590a33ed
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-02-28 11:28:51 +1300

    Issue #38 Use node-fetch to prevent unit test errors due to missing global fetch (apollo-graphql)

--- src/utils/graphql.js
+++ src/utils/graphql.js
@@ -1,10 +1,13 @@
 // import { createProvider } from './vue-apollo'
 import ApolloClient from 'apollo-boost'
+import fetch from 'node-fetch'
 
 // GraphQL
 const apolloClient = new ApolloClient({
   // You should use an absolute URL here
-  uri: "https://api.graphcms.com/simple/v1/awesomeTalksClone"
-})
+  uri: "https://api.graphcms.com/simple/v1/awesomeTalksClone",
+  // we must define fetch, as otherwise running unit tests it would fail as there is no global fetch variable
+  fetch: fetch
+});
 
 export default apolloClient
