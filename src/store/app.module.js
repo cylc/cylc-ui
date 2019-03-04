fc41c1ef9d43488439d4d625712d9ce867e8a62c
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-03-04 14:27:34 +1300

    Organize methods

--- src/store/app.module.js
+++ src/store/app.module.js
@@ -3,20 +3,20 @@ import { set, toggle } from '@/utils/vuex'
 const state = {
   drawer: null,
   color: 'success'
-}
-
-const actions = {}
+};
 
 const mutations = {
   setDrawer: set('drawer'),
   setImage: set('image'),
   setColor: set('color'),
   toggleDrawer: toggle('drawer')
-}
+};
+
+const actions = {};
 
 export const app = {
   namespaced: true,
   state,
   actions,
   mutations
-}
+};
