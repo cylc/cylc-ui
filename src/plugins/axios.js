1a1eb9ab6639746c79a3c8f9c3388128898bdd0c
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-03-05 15:03:07 +1300

    Issue #40 Add setLoading calls in interceptors for Axios

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,22 @@
 import Vue from 'vue'
+import store from '@/store/'
 
 // Lib imports
 import axios from 'axios'
 
-Vue.prototype.$http = axios
+axios.interceptors.request.use(function(config) {
+  store.dispatch('setLoading', true);
+  return config;
+}, function(error) {
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(function(response) {
+  store.dispatch('setLoading', false);
+  return response;
+}, function(error) {
+  store.dispatch('setLoading', false);
+  return Promise.reject(error);
+});
+
+Vue.prototype.$http = axios;
