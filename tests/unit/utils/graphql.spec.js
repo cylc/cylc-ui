73ae0cff42343d7b3823de01cf522019a2afc022
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-03-08 12:03:29 +1300

    Add unit tests for utils/graphql

---
+++ tests/unit/utils/graphql.spec.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai'
+import { createApolloClient } from '@/utils/graphql'
+
+describe('utils', () => {
+  describe('graphql', () => {
+    it('should create an apollo client', () => {
+      const uri = 'http://localhost:12345';
+      const apolloClient = createApolloClient(uri);
+      expect(apolloClient.link !== null).to.equal(true);
+    })
+  });
+});
