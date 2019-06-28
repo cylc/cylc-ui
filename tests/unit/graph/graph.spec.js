409e6aba72df113f4409d04f080c2757b205750d
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-06-29 00:02:47 +1200

    Mount component once at test, avoiding timeouts

--- tests/unit/graph/graph.spec.js
+++ tests/unit/graph/graph.spec.js
@@ -15,43 +15,37 @@ Vue.use(
 )
 
 describe('Graph', () => {
+  const wrapper = mount(Graph)
+
   it('should mount for testing', () => {
-    const wrapper = mount(Graph)
     expect(wrapper.find('[id="holder"]'))
   })
 
   it('should create the cytoscape element', () => {
-    const wrapper = mount(Graph)
     expect(wrapper.find('[id="cytoscape"]'))
   })
 
   it('should create the loading spinner', () => {
-    const wrapper = mount(Graph)
     expect(wrapper.find('[id="spinner"]'))
   })
 
   it('should create the switch layout buttons div', () => {
-    const wrapper = mount(Graph)
     expect(wrapper.find('[class="switchlayout"]'))
   })
 
   it('should create the cytoscape-navigator-view', () => {
-    const wrapper = mount(Graph)
     expect(wrapper.find('[class="cytoscape-navigatorView"]'))
   })
 
   it('should create the cytoscape-navigator-overlay', () => {
-    const wrapper = mount(Graph)
     expect(wrapper.find('[class="cytoscape-navigatorOverlay"]'))
   })
 
   it('should create the cytoscape-navigatorView', () => {
-    const wrapper = mount(Graph)
     expect(wrapper.find('[class="cytoscape-navigatorView"]'))
   })
 
   it('should load the panzoom extension', () => {
-    const wrapper = mount(Graph)
     expect(wrapper.find('[class="cy-panzoom"]'))
   })
 })
