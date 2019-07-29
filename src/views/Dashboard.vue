1a3f7bb0960a22ac75702b3ba8e3511a94441864
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-07-29 13:56:28 +1200

    Using Task and Job components to render the dashboard nodes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -34,14 +34,107 @@
         </v-data-table>
       </v-flex>
     </v-layout>
+    <v-layout row wrap>
+      <v-flex xs6 md6 lg2>
+        <v-list two-line>
+          <v-list-tile avatar>
+            <v-list-tile-avatar size="80" style="font-size: 2em;">
+              <task status="succeeded" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title class="title font-weight-thin">
+                Settings
+              </v-list-tile-title>
+              <v-list-tile-sub-title>
+                <job status="succeeded" />
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile avatar>
+            <v-list-tile-avatar size="80" style="font-size: 2em;">
+              <task status="waiting" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title class="title font-weight-thin">
+                Cylc Hub
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+      <v-flex xs6 md6 lg2>
+        <v-list two-line>
+          <v-list-tile avatar>
+            <v-list-tile-avatar size="80" style="font-size: 2em;">
+              <task status="succeeded" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title class="title font-weight-thin">
+                Cylc UI Quickstart
+              </v-list-tile-title>
+              <v-list-tile-sub-title>
+                <job status="succeeded" />
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile avatar>
+            <v-list-tile-avatar size="80" style="font-size: 2em;">
+              <task status="running" :progress=25 />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title class="title font-weight-thin">
+                Documentation
+              </v-list-tile-title>
+              <v-list-tile-sub-title>
+                <job status="submitted"/>
+                <job status="submitted"/>
+                <job status="submitted"/>
+              </v-list-tile-sub-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile avatar>
+            <v-list-tile-avatar size="80" style="font-size: 2em;">
+              <task status="waiting" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title class="title font-weight-thin">
+                Suite Design Guide
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex xs12 md12 lg4>
+        <v-list align-center>
+          <v-list-tile>
+            <v-list-tile-avatar size="80" style="font-size: 2em;">
+              <task status="failed" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title class="title font-weight-thin">
+                Stop Server
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-flex>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 import { mixin } from '@/mixins/index'
+import Task from '@/components/cylc/Task'
+import Job from '@/components/cylc/Job'
 
 export default {
   mixins: [mixin],
+  components: {
+    task: Task,
+    job: Job
+  },
   metaInfo () {
     return {
       title: this.getPageTitle('App.dashboard')
@@ -57,9 +150,3 @@ export default {
   })
 }
 </script>
-
-<style>
-.c-dashboard {
-  font-size: 120%;
-}
-</style>
