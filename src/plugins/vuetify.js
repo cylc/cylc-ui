2b3348ae9e27b7abe3b2392dda680b0b9093ffd2
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-08-27 11:10:55 +1200

    Update styles for new Vuetify (no theme scss vars, instead use globals)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
-import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
 
-Vue.use(Vuetify, {
+Vue.use(Vuetify)
+
+export default new Vuetify({
   theme: {
     themes: {
       light: {
@@ -18,5 +19,7 @@ Vue.use(Vuetify, {
       }
     }
   },
-  iconfont: 'mdi'
+  icons: {
+    iconfont: 'mdi'
+  }
 })
