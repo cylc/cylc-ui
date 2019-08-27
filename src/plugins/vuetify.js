7cc3e446fcd8de2af705ea6f1e8c624010a10863
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-08-28 09:04:58 +1200

    Disable theme from vuetify (we will have our own implementation)

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,20 +8,7 @@ import '@mdi/font/css/materialdesignicons.css'
 Vue.use(Vuetify)
 
 export default new Vuetify({
-  theme: {
-    themes: {
-      light: {
-        primary: '#b3d4fc', // from design sketch
-        secondary: '#4caf50',
-        tertiary: '#495057',
-        accent: '#82B1FF',
-        error: '#f55a4e',
-        info: '#00d3ee',
-        success: '#5cb860',
-        warning: '#ffa21a'
-      }
-    }
-  },
+  theme: { disable: true },
   icons: {
     iconfont: 'mdi'
   }
