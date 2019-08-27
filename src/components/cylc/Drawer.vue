7b9534c05b5fbbc9c17f3f2015e5f163264ac83a
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-08-27 14:46:41 +1200

    Fix styles in Drawer for latest vuetify

---
+++ src/components/cylc/Drawer.vue
@@ -0,0 +1,157 @@
+<template>
+  <v-navigation-drawer
+    id="app-drawer"
+    v-model="inputValue"
+    app
+    persistent
+    floating
+    mobile-break-point="991"
+    width="260"
+    clipped
+  >
+    <v-layout
+      tag="v-list"
+      column
+    >
+      <c-header class="fill-height" />
+      <v-list-item
+        v-if="responsive"
+      >
+        <v-text-field
+          class="purple-input search-input"
+          label="Search..."
+          color="purple"
+        />
+      </v-list-item>
+
+      <v-list-item
+          to="/dashboard"
+          active-class="primary white--text"
+          class="v-list-item"
+      >
+        <v-list-item-action>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-action>
+        <v-list-item-title>Dashboard</v-list-item-title>
+      </v-list-item>
+      <v-subheader>Views</v-subheader>
+      <v-list-item
+        v-for="(link, index) in viewLinks"
+        :key="index+link.text"
+        :to="link.to"
+        active-class="primary white--text"
+        class="v-list-item"
+      >
+        <v-list-item-action>
+          <v-icon>{{ link.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-title
+          v-text="link.text"
+        />
+      </v-list-item>
+    </v-layout>
+  </v-navigation-drawer>
+</template>
+
+<script>
+// Utilities
+import {
+  mapMutations
+} from 'vuex'
+// because we use `tag=v-list` and not `v-list`
+// eslint-disable-next-line no-unused-vars
+import { VList } from 'vuetify/lib/components/VList'
+import Header from '@/components/cylc/Header'
+import i18n from '@/i18n'
+
+export default {
+  components: {
+    'c-header': Header,
+    // eslint-disable-next-line vue/no-unused-components
+    'v-list': VList
+  },
+  data: () => ({
+    links: [
+      {
+        to: '/',
+        icon: 'mdi-view-dashboard',
+        text: i18n.t('App.dashboard'),
+        view: false
+      },
+      {
+        to: '/workflows',
+        icon: 'mdi-vector-circle',
+        text: i18n.t('App.workflows'),
+        view: true
+      },
+      {
+        to: '/graph',
+        icon: 'mdi-vector-polyline',
+        text: i18n.t('App.graph'),
+        view: true
+      }
+    ],
+    responsive: false
+  }),
+  computed: {
+    inputValue: {
+      get () {
+        return this.$store.state.app.drawer
+      },
+      set (val) {
+        this.setDrawer(val)
+      }
+    },
+    viewLinks: function () {
+      return this.isView(true)
+    },
+    nonViewLinks: function () {
+      return this.isView(false)
+    },
+    items () {
+      return this.$t('Layout.View.items')
+    }
+  },
+  methods: {
+    ...mapMutations('app', ['setDrawer', 'toggleDrawer']),
+    isView (bool) {
+      // return links to views for true argument, non-views for false argument
+      return this.links.filter(function (u) {
+        return u.view === bool
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  #app-drawer {
+    .v-list__tile {
+      border-radius: 4px;
+      margin-top: 5px;
+
+      &--buy {
+        margin-top: auto;
+        margin-bottom: auto;
+      }
+    }
+
+    .v-image__image--contain {
+      top: 30px;
+      bottom: 30px;
+      height: 60%;
+    }
+
+    .search-input {
+      margin-bottom: 30px !important;
+      padding-left: 15px;
+      padding-right: 15px;
+    }
+  }
+
+  /* this is not in our styles/material-dashboard, so we need to force-override */
+  .v-navigation-drawer {
+    -webkit-box-shadow: none !important;
+    box-shadow: none !important;
+  }
+</style>
