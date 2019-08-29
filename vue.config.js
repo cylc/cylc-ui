eb19d6143dddae0812f7d1443255c1f73857d931
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-08-29 14:03:15 +1200

    Ignore sass-loader for tests in mocha (due to vuetify update)

--- vue.config.js
+++ vue.config.js
@@ -29,6 +29,17 @@ module.exports = {
       config.devtool('inline-cheap-module-source-map')
     }
 
+    // mocha-webpack appears to be having issues with sass-loader, interpreting .sass files as .scss after
+    // the update from vuetify 1.5.x to 2.x (they changed from node-sass to sass). Their example contains
+    // a jest configuration file that prevents some transformations, and uses their own plugins for others.
+    // Troubleshooting it during the update, it appears that the only way to make our tests to pass was
+    // by ignoring sass transformations too in webpack.
+    if (process.env.NODE_ENV === 'test') {
+      config.module.rule('sass')
+        .use('sass')
+        .loader('null-loader')
+    }
+
     // set up aliases for mock services, used when the offline mode is used
     const workflowService = process.env.NODE_ENV === 'offline'
       ? '@/services/mock/workflow.service.mock'
