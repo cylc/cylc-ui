c8fae392692a9397d74995b99bd23b071fd9eb58
Hilary James Oliver <hilary.j.oliver@gmail.com>
2019-09-16 13:32:51 +1200

    Fix progress function.

--- src/store/workflows.module.js
+++ src/store/workflows.module.js
@@ -45,21 +45,21 @@ function _getCycles (workflows) {
  *
  * @see https://github.com/cylc/cylc-flow/blob/de7d938496e82dbdfb165938145670dd8e801efd/lib/cylc/gui/updater_tree.py#L248-L263
  * @param {number} startedTime in milliseconds since 1970-01-01 00:00:00 UTC, e.g. 1568353099874
- * @param {number} meanElapsedTime mean elapsed time (e.g. 13.5)
+ * @param {number} meanElapsedTime mean elapsed time in seconds
  * @returns {number} the percent progress, e.g. 25 (meaning 25% progress)
  * @private
  */
 function _computePercentProgress (startedTime, meanElapsedTime) {
-  const now = Date.now()
-  // startedTime > now reportably possible via interaction with `cylc reset`
+  const now = Date.now() // milliseconds since 1970-01-01
+  // startedTime > now reportedly possible via interaction with `cylc reset`
   if (startedTime > now || meanElapsedTime === 0) {
     return 0
   }
-  const elapsedTime = startedTime + meanElapsedTime
-  if (now > elapsedTime) {
+
+  if (now > startedTime + meanElapsedTime * 1000) {
     return 100
   }
-  return 100 * (now - startedTime) / meanElapsedTime
+  return 100 * (now - startedTime) / (meanElapsedTime * 1000)
 }
 
 /**
