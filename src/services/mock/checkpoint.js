097325be8ccedf12a639f0d012490350d6ba1bd6
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-09-16 16:50:53 +1200

    Update mocked test workflow, and prevent eslint errors

--- src/services/mock/checkpoint.js
+++ src/services/mock/checkpoint.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 const checkpoint = {
     "workflows": [
         {
@@ -6,8 +8,18 @@ const checkpoint = {
             "status": "running",
             "owner": "cylc",
             "host": "ranma",
-            "port": 43082,
+            "port": 43043,
             "taskProxies": [
+                {
+                    "id": "cylc|one|20000102T0000Z|waiting",
+                    "state": "waiting",
+                    "cyclePoint": "20000102T0000Z",
+                    "task": {
+                        "meanElapsedTime": 1.0,
+                        "name": "waiting"
+                    },
+                    "jobs": []
+                },
                 {
                     "id": "cylc|one|20000101T0000Z|succeeded",
                     "state": "succeeded",
@@ -20,35 +32,7 @@ const checkpoint = {
                         {
                             "id": "cylc|one|20000101T0000Z|succeeded|1",
                             "host": "localhost",
-                            "startedTime": "2019-09-16T04:21:38Z",
-                            "state": "succeeded",
-                            "submitNum": 1
-                        }
-                    ]
-                },
-                {
-                    "id": "cylc|one|20000102T0000Z|sleepy",
-                    "state": "waiting",
-                    "cyclePoint": "20000102T0000Z",
-                    "task": {
-                        "meanElapsedTime": 1.0,
-                        "name": "sleepy"
-                    },
-                    "jobs": []
-                },
-                {
-                    "id": "cylc|one|20000102T0000Z|succeeded",
-                    "state": "succeeded",
-                    "cyclePoint": "20000102T0000Z",
-                    "task": {
-                        "meanElapsedTime": 1.0,
-                        "name": "succeeded"
-                    },
-                    "jobs": [
-                        {
-                            "id": "cylc|one|20000102T0000Z|succeeded|1",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:09Z",
+                            "startedTime": "2019-09-16T04:46:00Z",
                             "state": "succeeded",
                             "submitNum": 1
                         }
@@ -66,167 +50,7 @@ const checkpoint = {
                         {
                             "id": "cylc|one|20000101T0000Z|retrying|1",
                             "host": "localhost",
-                            "startedTime": "2019-09-16T04:21:38Z",
-                            "state": "failed",
-                            "submitNum": 1
-                        }
-                    ]
-                },
-                {
-                    "id": "cylc|one|20000102T0000Z|waiting",
-                    "state": "waiting",
-                    "cyclePoint": "20000102T0000Z",
-                    "task": {
-                        "meanElapsedTime": 1.0,
-                        "name": "waiting"
-                    },
-                    "jobs": []
-                },
-                {
-                    "id": "cylc|one|20000102T0000Z|failed",
-                    "state": "failed",
-                    "cyclePoint": "20000102T0000Z",
-                    "task": {
-                        "meanElapsedTime": 0.0,
-                        "name": "failed"
-                    },
-                    "jobs": [
-                        {
-                            "id": "cylc|one|20000102T0000Z|failed|1",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:24Z",
-                            "state": "failed",
-                            "submitNum": 1
-                        }
-                    ]
-                },
-                {
-                    "id": "cylc|one|20000101T0000Z|failed",
-                    "state": "failed",
-                    "cyclePoint": "20000101T0000Z",
-                    "task": {
-                        "meanElapsedTime": 0.0,
-                        "name": "failed"
-                    },
-                    "jobs": [
-                        {
-                            "id": "cylc|one|20000101T0000Z|failed|1",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:21:53Z",
-                            "state": "failed",
-                            "submitNum": 1
-                        }
-                    ]
-                },
-                {
-                    "id": "cylc|one|20000101T0000Z|eventually_succeeded",
-                    "state": "succeeded",
-                    "cyclePoint": "20000101T0000Z",
-                    "task": {
-                        "meanElapsedTime": 1.0,
-                        "name": "eventually_succeeded"
-                    },
-                    "jobs": [
-                        {
-                            "id": "cylc|one|20000101T0000Z|eventually_succeeded|4",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:21:50Z",
-                            "state": "succeeded",
-                            "submitNum": 4
-                        },
-                        {
-                            "id": "cylc|one|20000101T0000Z|eventually_succeeded|3",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:21:46Z",
-                            "state": "failed",
-                            "submitNum": 3
-                        },
-                        {
-                            "id": "cylc|one|20000101T0000Z|eventually_succeeded|2",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:21:42Z",
-                            "state": "failed",
-                            "submitNum": 2
-                        },
-                        {
-                            "id": "cylc|one|20000101T0000Z|eventually_succeeded|1",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:21:38Z",
-                            "state": "failed",
-                            "submitNum": 1
-                        }
-                    ]
-                },
-                {
-                    "id": "cylc|one|20000101T0000Z|sleepy",
-                    "state": "succeeded",
-                    "cyclePoint": "20000101T0000Z",
-                    "task": {
-                        "meanElapsedTime": 1.0,
-                        "name": "sleepy"
-                    },
-                    "jobs": [
-                        {
-                            "id": "cylc|one|20000101T0000Z|sleepy|1",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:05Z",
-                            "state": "succeeded",
-                            "submitNum": 1
-                        }
-                    ]
-                },
-                {
-                    "id": "cylc|one|20000102T0000Z|checkpoint",
-                    "state": "running",
-                    "cyclePoint": "20000102T0000Z",
-                    "task": {
-                        "meanElapsedTime": 6.0,
-                        "name": "checkpoint"
-                    },
-                    "jobs": [
-                        {
-                            "id": "cylc|one|20000102T0000Z|checkpoint|1",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:27Z",
-                            "state": "running",
-                            "submitNum": 1
-                        }
-                    ]
-                },
-                {
-                    "id": "cylc|one|20000102T0000Z|eventually_succeeded",
-                    "state": "succeeded",
-                    "cyclePoint": "20000102T0000Z",
-                    "task": {
-                        "meanElapsedTime": 1.0,
-                        "name": "eventually_succeeded"
-                    },
-                    "jobs": [
-                        {
-                            "id": "cylc|one|20000102T0000Z|eventually_succeeded|4",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:21Z",
-                            "state": "succeeded",
-                            "submitNum": 4
-                        },
-                        {
-                            "id": "cylc|one|20000102T0000Z|eventually_succeeded|3",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:17Z",
-                            "state": "failed",
-                            "submitNum": 3
-                        },
-                        {
-                            "id": "cylc|one|20000102T0000Z|eventually_succeeded|2",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:13Z",
-                            "state": "failed",
-                            "submitNum": 2
-                        },
-                        {
-                            "id": "cylc|one|20000102T0000Z|eventually_succeeded|1",
-                            "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:09Z",
+                            "startedTime": "2019-09-16T04:46:00Z",
                             "state": "failed",
                             "submitNum": 1
                         }
@@ -244,8 +68,129 @@ const checkpoint = {
                         {
                             "id": "cylc|one|20000101T0000Z|waiting|1",
                             "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:05Z",
+                            "startedTime": "2019-09-16T04:46:27Z",
                             "state": "succeeded",
+                            "submitNum": 1
+                        }
+                    ]
+                },
+                {
+                    "id": "cylc|one|20000102T0000Z|checkpoint",
+                    "state": "running",
+                    "cyclePoint": "20000102T0000Z",
+                    "task": {
+                        "meanElapsedTime": 6.0,
+                        "name": "checkpoint"
+                    },
+                    "jobs": [
+                        {
+                            "id": "cylc|one|20000102T0000Z|checkpoint|1",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:49Z",
+                            "state": "running",
+                            "submitNum": 1
+                        }
+                    ]
+                },
+                {
+                    "id": "cylc|one|20000102T0000Z|succeeded",
+                    "state": "succeeded",
+                    "cyclePoint": "20000102T0000Z",
+                    "task": {
+                        "meanElapsedTime": 1.0,
+                        "name": "succeeded"
+                    },
+                    "jobs": [
+                        {
+                            "id": "cylc|one|20000102T0000Z|succeeded|1",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:31Z",
+                            "state": "succeeded",
+                            "submitNum": 1
+                        }
+                    ]
+                },
+                {
+                    "id": "cylc|one|20000102T0000Z|sleepy",
+                    "state": "waiting",
+                    "cyclePoint": "20000102T0000Z",
+                    "task": {
+                        "meanElapsedTime": 1.0,
+                        "name": "sleepy"
+                    },
+                    "jobs": []
+                },
+                {
+                    "id": "cylc|one|20000101T0000Z|sleepy",
+                    "state": "succeeded",
+                    "cyclePoint": "20000101T0000Z",
+                    "task": {
+                        "meanElapsedTime": 1.0,
+                        "name": "sleepy"
+                    },
+                    "jobs": [
+                        {
+                            "id": "cylc|one|20000101T0000Z|sleepy|1",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:27Z",
+                            "state": "succeeded",
+                            "submitNum": 1
+                        }
+                    ]
+                },
+                {
+                    "id": "cylc|one|20000101T0000Z|eventually_succeeded",
+                    "state": "succeeded",
+                    "cyclePoint": "20000101T0000Z",
+                    "task": {
+                        "meanElapsedTime": 0.5,
+                        "name": "eventually_succeeded"
+                    },
+                    "jobs": [
+                        {
+                            "id": "cylc|one|20000101T0000Z|eventually_succeeded|4",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:12Z",
+                            "state": "succeeded",
+                            "submitNum": 4
+                        },
+                        {
+                            "id": "cylc|one|20000101T0000Z|eventually_succeeded|3",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:08Z",
+                            "state": "failed",
+                            "submitNum": 3
+                        },
+                        {
+                            "id": "cylc|one|20000101T0000Z|eventually_succeeded|2",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:04Z",
+                            "state": "failed",
+                            "submitNum": 2
+                        },
+                        {
+                            "id": "cylc|one|20000101T0000Z|eventually_succeeded|1",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:00Z",
+                            "state": "failed",
+                            "submitNum": 1
+                        }
+                    ]
+                },
+                {
+                    "id": "cylc|one|20000102T0000Z|failed",
+                    "state": "failed",
+                    "cyclePoint": "20000102T0000Z",
+                    "task": {
+                        "meanElapsedTime": 0.0,
+                        "name": "failed"
+                    },
+                    "jobs": [
+                        {
+                            "id": "cylc|one|20000102T0000Z|failed|1",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:46Z",
+                            "state": "failed",
                             "submitNum": 1
                         }
                     ]
@@ -262,8 +207,65 @@ const checkpoint = {
                         {
                             "id": "cylc|one|20000101T0000Z|checkpoint|1",
                             "host": "localhost",
-                            "startedTime": "2019-09-16T04:21:56Z",
+                            "startedTime": "2019-09-16T04:46:18Z",
                             "state": "succeeded",
+                            "submitNum": 1
+                        }
+                    ]
+                },
+                {
+                    "id": "cylc|one|20000101T0000Z|failed",
+                    "state": "failed",
+                    "cyclePoint": "20000101T0000Z",
+                    "task": {
+                        "meanElapsedTime": 0.0,
+                        "name": "failed"
+                    },
+                    "jobs": [
+                        {
+                            "id": "cylc|one|20000101T0000Z|failed|1",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:15Z",
+                            "state": "failed",
+                            "submitNum": 1
+                        }
+                    ]
+                },
+                {
+                    "id": "cylc|one|20000102T0000Z|eventually_succeeded",
+                    "state": "succeeded",
+                    "cyclePoint": "20000102T0000Z",
+                    "task": {
+                        "meanElapsedTime": 0.5,
+                        "name": "eventually_succeeded"
+                    },
+                    "jobs": [
+                        {
+                            "id": "cylc|one|20000102T0000Z|eventually_succeeded|4",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:43Z",
+                            "state": "succeeded",
+                            "submitNum": 4
+                        },
+                        {
+                            "id": "cylc|one|20000102T0000Z|eventually_succeeded|3",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:39Z",
+                            "state": "failed",
+                            "submitNum": 3
+                        },
+                        {
+                            "id": "cylc|one|20000102T0000Z|eventually_succeeded|2",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:35Z",
+                            "state": "failed",
+                            "submitNum": 2
+                        },
+                        {
+                            "id": "cylc|one|20000102T0000Z|eventually_succeeded|1",
+                            "host": "localhost",
+                            "startedTime": "2019-09-16T04:46:31Z",
+                            "state": "failed",
                             "submitNum": 1
                         }
                     ]
@@ -280,7 +282,7 @@ const checkpoint = {
                         {
                             "id": "cylc|one|20000102T0000Z|retrying|1",
                             "host": "localhost",
-                            "startedTime": "2019-09-16T04:22:09Z",
+                            "startedTime": "2019-09-16T04:46:31Z",
                             "state": "failed",
                             "submitNum": 1
                         }
