917a1662a8f4c26c2c592d70804c64bc4c43547a
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-10-24 23:10:14 +1300

    Fix promise (was returning another promise, instead of chaining)

--- src/services/mock/user.service.mock.js
+++ src/services/mock/user.service.mock.js
@@ -4,10 +4,15 @@ import store from '@/store/index'
 export const UserService = {
 
   getUserProfile () {
-    return new Promise((resolve, reject) => {
-      const user = new User(process.env.USER, ['cylc-developers', 'linux-users'], new Date(), true, `/user/${process.env.USER}/`)
-      return store.dispatch('user/setUser', user)
-    })
+    const username = 'cylc'
+    const user = new User(
+      username,
+      ['cylc-developers', 'linux-users'],
+      new Date(),
+      true,
+      `/user/${username}/`
+    )
+    return store.dispatch('user/setUser', user)
   }
 
 }
