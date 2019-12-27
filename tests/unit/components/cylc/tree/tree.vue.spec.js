44f03b7cbc891ca327331f59f3aeb4cec058d42f
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2019-12-27 16:15:48 +1300

    Remove old tests for Tree for features that are not implemented for the new tree (activable, minimum depth)

--- tests/unit/components/cylc/tree/tree.vue.spec.js
+++ tests/unit/components/cylc/tree/tree.vue.spec.js
@@ -16,66 +16,36 @@
  */
 
 // we mount the tree to include the TreeItem component and other vuetify children components
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import { expect } from 'chai'
-import Vue from 'vue'
 // import vuetify here so that we do not have warnings in the console output
 // eslint-disable-next-line no-unused-vars
 import * as vuetify from '@/plugins/vuetify'
 import Tree from '@/components/cylc/tree/Tree'
 import { simpleWorkflowTree4Nodes } from './tree.data'
 
-const cycles = new Map([
-  [
-    'user/workflow1',
-    new Set([
-      '20100101T0000Z'
-    ])
-  ]
-])
+/// vue-observe-visibility transitive dependency requires it
+global.IntersectionObserver = class IntersectionObserver {
+  // eslint-disable-next-line no-useless-constructor
+  constructor () {}
 
-describe.skip('Tree component', () => {
+  observe () {
+    return null
+  }
+
+  unobserve () {
+    return null
+  }
+}
+
+describe('Tree component', () => {
   it('should display the tree with valid data', () => {
-    const wrapper = mount(Tree, {
+    // eslint-disable-next-line no-unused-vars
+    const wrapper = shallowMount(Tree, {
       propsData: {
-        workflows: simpleWorkflowTree4Nodes[0].children
+        treeData: simpleWorkflowTree4Nodes
       }
     })
-    expect(wrapper.props().workflows[0].node.__typename).to.equal('CyclePoint')
-    expect(wrapper.contains('div')).to.equal(true)
-  })
-  describe('activable', () => {
-    it('should not activate by default', () => {
-      const wrapper = mount(Tree, {
-        propsData: {
-          workflows: simpleWorkflowTree4Nodes[0].children
-        }
-      })
-      const treeItems = wrapper.findAll({ name: 'TreeItem' })
-      const workflowTreeItem = treeItems.wrappers[0]
-      // the workflow tree item node must not be active
-      const workflowTreeItemNode = workflowTreeItem.find('div.node')
-      expect(workflowTreeItemNode.classes('node--active')).to.equal(false)
-      const workflowTreeItemNodeActivableSpan = workflowTreeItemNode.find('.node-data > span')
-      workflowTreeItemNodeActivableSpan.trigger('click')
-      expect(workflowTreeItemNode.classes('node--active')).to.equal(false)
-    })
-    it('should activate correctly', async () => {
-      const wrapper = mount(Tree, {
-        propsData: {
-          workflows: simpleWorkflowTree4Nodes[0].children,
-          activable: true
-        }
-      })
-      const treeItems = wrapper.findAll({ name: 'TreeItem' })
-      const workflowTreeItem = treeItems.wrappers[0]
-      // the workflow tree item node must not be active
-      const workflowTreeItemNode = workflowTreeItem.find('div.node')
-      expect(workflowTreeItemNode.classes('node--active')).to.equal(false)
-      const workflowTreeItemNodeActivableSpan = workflowTreeItemNode.find('.node-data > span')
-      workflowTreeItemNodeActivableSpan.trigger('click')
-      await Vue.nextTick()
-      expect(workflowTreeItemNode.classes('node--active')).to.equal(true)
-    })
+    expect(wrapper.props().treeData[0].__type).to.equal('workflow')
   })
 })
