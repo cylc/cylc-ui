8ad1fb9972b3beae69f0efa5fb08b0cb8870851d
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2020-01-09 10:45:51 +1300

    Move queries out of the Graph component

--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -5,6 +5,49 @@
 <script>
 import Graph from '@/components/cylc/Graph'
 
+const QUERIES = {
+  root: `
+    subscription {
+      workflows(ids: ["WORKFLOW_ID"]) {
+        id
+        status
+        nodesEdges {
+          nodes {
+            id
+            label: id
+            parent: firstParent {
+              id
+              state
+            }
+            state
+            cyclePoint
+            task {
+              name
+            }
+            jobs(sort: {keys: ["submit_num"], reverse: true}) {
+              id
+              batchSysName
+              batchSysJobId
+              host
+              startedTime
+              submittedTime
+              finishedTime
+              state
+              submitNum
+            }
+          }
+          edges {
+            id
+            source
+            target
+            label: id
+          }
+        }
+      }
+    }
+  `
+}
+
 export default {
   components: {
     Graph
@@ -14,6 +57,56 @@ export default {
     workflowName: {
       type: String,
       required: true
+    }
+  },
+
+  data: () => ({
+    viewID: '',
+    subscriptions: {},
+    isLoading: true
+  }),
+
+  created () {
+    this.viewID = `Graph(${this.workflowName}): ${Math.random()}`
+    this.$workflowService.register(
+      this,
+      {
+        activeCallback: this.setActive
+      }
+    )
+    this.subscribe('root')
+  },
+
+  beforeDestroy () {
+    this.$workflowService.unregister(this)
+  },
+
+  methods: {
+    subscribe (queryName) {
+      const id = this.$workflowService.subscribe(
+        this,
+        QUERIES[queryName].replace('WORKFLOW_ID', this.workflowName)
+      )
+      if (!(queryName in this.subscriptions)) {
+        this.subscriptions[queryName] = {
+          id
+        }
+      }
+    },
+
+    unsubscribe (queryName) {
+      if (queryName in this.subscriptions) {
+        this.$workflowService.unsubscribe(
+          this.subscriptions[queryName].id
+        )
+      }
+    },
+
+    setActive (isActive) {
+      /** Toggle the isLoading state.
+       * @param {bool} isActive - Are this views subs active.
+       */
+      this.isLoading = !isActive
     }
   }
 }
