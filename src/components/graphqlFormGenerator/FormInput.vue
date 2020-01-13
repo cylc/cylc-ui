7c9382632700e440becf09e4265f4fff232a02e4
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-01-13 17:43:39 +1300

    aotf: use mixins to keep code d[n]ry

--- src/components/graphqlFormGenerator/FormInput.vue
+++ src/components/graphqlFormGenerator/FormInput.vue
@@ -13,8 +13,10 @@
 import Vue from 'vue'
 
 import { VTextField } from 'vuetify/lib/components/VTextField'
-import GList from '@/components/graphqlFormGenerator/components/List'
+
+import { formElement } from '@/components/graphqlFormGenerator/mixins'
 import GNonNull from '@/components/graphqlFormGenerator/components/NonNull'
+import GList from '@/components/graphqlFormGenerator/components/List'
 
 /* Vuetify number input component.
  *
@@ -67,32 +69,20 @@ const NAMED_TYPES = {
 // registry of GraphQL "kinds" (e.g. LIST)
 // {namedType: {is: ComponentClass, prop1: value, ...}}
 const KINDS = {
-  LIST: {
-    is: GList
-  },
   NON_NULL: {
     is: GNonNull
+  },
+  LIST: {
+    is: GList
   }
 }
 
 export default {
-  name: 'form-input',
+  name: 'g-form-input',
+
+  mixins: [formElement],
 
   props: {
-    // the GraphQL type this input represents
-    gqlType: {
-      type: Object,
-      required: true
-    },
-    // the form label for this input
-    label: {
-      type: String,
-      required: true
-    },
-    // the value (v-model is actually syntactic sugar for this)
-    value: {
-      required: true
-    },
     // dictionary of props for overriding default values
     propOverrides: {
       type: Object,
@@ -105,22 +95,6 @@ export default {
   }),
 
   computed: {
-    /* The model we pass to the form input.
-     *
-     * Note the v-model approach does not work with nesting out of the box,
-     * you need to capture and propagate "input" events up the component tree
-     * to enable this nested structure of components to share the same model
-     * and be managed by Vue correctly.
-     */
-    model: {
-      get () {
-        return this.value
-      },
-      set (val) {
-        this.$emit('input', val)
-      }
-    },
-
     /* The props to pass to the form input.
      *
      * Note, this includes the "is" prop which tells Vue which component class
