e51c9e0d71f1cc179ad7fac0a084972c44949342
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2020-01-23 09:58:48 +1300

    aotf: use v-inupt for object component

--- src/components/graphqlFormGenerator/components/Object.vue
+++ src/components/graphqlFormGenerator/components/Object.vue
@@ -1,22 +1,19 @@
 <template>
-  <div>
-    <v-container
-     class="grey lighten-2"
-    >
-      <p>{{ type.name }}</p>
-      <v-row>
-        <component
-         v-for="input in inputs"
-         v-bind:key="input.label"
-         v-model="model[input.label]"
-         :is="FormInput"
-         :gqlType="input.gqlType"
-         :types="types"
-         :label="input.label"
-        />
-      </v-row>
-    </v-container>
-  </div>
+  <v-input
+   v-bind="props"
+  >
+    <template>
+      <component
+       v-for="input in inputs"
+       v-bind:key="input.label"
+       v-model="model[input.label]"
+       :is="FormInput"
+       :gqlType="input.gqlType"
+       :types="types"
+       :label="input.label"
+      />
+    </template>
+  </v-input>
 </template>
 
 <script>
