90f2a017466d199dfba05683b076ab031f466e4c
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2020-03-11 16:14:46 +1300

    Update user profile tests, removing TODO now that the offline mode is working

--- tests/e2e/specs/userprofile.js
+++ tests/e2e/specs/userprofile.js
@@ -1,12 +1,12 @@
 describe('User Profile', () => {
   it('Visits the user profile', () => {
     cy.visit('/#/user-profile')
-    cy.get('h4.title')
+    cy
+      .get('h4.title')
       .should('be.visible')
       .should('contain', 'Your Profile')
-    // TODO: offline mode for UserService
-    // cy.get('input#username')
-    //   .should('be.visible')
-    //   .should('be.disabled')
+    cy.get('input#username')
+      .should('be.visible')
+      .should('be.disabled')
   })
 })
