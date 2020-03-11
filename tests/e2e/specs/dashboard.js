01e2bd660eb9d90f603851c57c0a91fb26dd2b12
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2020-03-11 16:14:08 +1300

    Use single level for tests (looks like Cypress does not work like Mocha)

--- tests/e2e/specs/dashboard.js
+++ tests/e2e/specs/dashboard.js
@@ -1,13 +1,11 @@
 describe('Dashboard', () => {
-  it('Visits the app root url', () => {
-    it('Displays the Dashboard link as active on the left sidebar menu', () => {
-      cy.visit('/#/')
-      cy
-        .get('div .v-list-item__title')
-        .contains('Dashboard')
-        .parent()
-        .should('have.class', 'v-list-item--active')
-    })
-    // TODO: add test that verifies the dashboard content after we have reviewed how it should look like
+  it('Displays the Dashboard link as active on the left sidebar menu', () => {
+    cy.visit('/#/')
+    cy
+      .get('div.v-list-item__title')
+      .contains('Dashboard')
+      .parent()
+      .should('have.class', 'v-list-item--active')
   })
+  // TODO: add test that verifies the dashboard content after we have reviewed how it should look like
 })
