239307831fbd4bec7f48ec252346202990720fbe
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2020-03-19 09:47:46 +1300

    Update test for new HTML element selector

--- tests/e2e/specs/userprofile.js
+++ tests/e2e/specs/userprofile.js
@@ -2,7 +2,7 @@ describe('User Profile', () => {
   it('Visits the user profile', () => {
     cy.visit('/#/user-profile')
     cy
-      .get('h4.title')
+      .get('h3.headline')
       .should('be.visible')
       .should('contain', 'Your Profile')
     cy.get('input#profile-username')
