58552de2343ca688270e60fb9349acac0722ea91
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-03-23 16:49:40 +1300

    Test that the leaf node is displayed and that its triangle has a margin-left

--- tests/e2e/specs/tree.js
+++ tests/e2e/specs/tree.js
@@ -34,7 +34,39 @@ describe('Tree component', () => {
       .get('.node-data-job:first')
       .should('be.visible')
   })
-  // it('Should display leaf node triangle with padding', () => {
-  //   // this is testing that there is a padding, not necessarily that the leaf node's triangle is exactly under the node
-  // }
+  it('Should display leaf node triangle with margin', () => {
+    // this is testing that there is a margin, not necessarily that the leaf node's triangle is exactly under the node
+    cy.visit('/#/workflows/one')
+    cy
+      .get('.node-data-task-proxy:first')
+      .prev()
+      .click()
+    // no jobs, and no leaves are visible initially
+    cy
+      .get('.leaf:first')
+      .should('not.be.visible')
+    // but clicking on a visible job should display its leaf node
+    cy
+      .get('.node-data-job:first')
+      .click()
+    cy
+      .get('.leaf:first')
+      .should('be.visible')
+    // and, important, the leaf node has a triangle, as a helper to quickly point the user to its parent
+    // job in the tree - i.e. the leaf has a left margin... as the leaves are not root nodes, we
+    // **always** have a margin > 0, unless a bug broke it (which happened before due to a wrong vairable name).
+    cy
+      .get('.leaf:first > .arrow-up')
+      .should(($div) => {
+        const marginLeft = $div.get(0).style.marginLeft
+        if (
+          marginLeft === undefined ||
+          marginLeft === '' ||
+          marginLeft === '0' ||
+          marginLeft === '0px'
+        ) {
+          throw new Error(`Invalid leaf node margin-left: "${marginLeft}"`)
+        }
+      })
+  })
 })
