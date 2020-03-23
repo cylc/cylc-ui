9f51cee80e03af6233772fd25778cd330566485e
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-03-23 16:17:38 +1300

    Confirm elements are visible too

--- tests/e2e/specs/tree.js
+++ tests/e2e/specs/tree.js
@@ -9,6 +9,9 @@ describe('Tree component', () => {
         expect($div.get(0)).to.contain('20000101T0000Z')
         expect($div.get(1)).to.contain('20000102T0000Z')
       })
+    cy
+      .get('.node-data-cyclepoint')
+      .should('be.visible')
   })
   // it('Should display leaf node triangle with padding', () => {
   //   // this is testing that there is a padding, not necessarily that the leaf node's triangle is exactly under the node
