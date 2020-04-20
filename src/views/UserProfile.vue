b7b1a9b3e2751ceb445e01154bae6f3ca44c2604
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-04-20 13:30:54 +1200

    Add form and buttons to user profile page

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -12,11 +12,13 @@
         </v-alert>
         <v-form v-if="user !== null">
           <v-container py-0>
-            <v-layout row wrap>
-              <v-flex xs12 md12>
+            <v-layout row align-center wrap>
+              <v-flex xs3>
+                <span>{{ $t('UserProfile.username') }}</span>
+              </v-flex>
+              <v-flex xs9>
                 <v-text-field
                     :value="user.username"
-                    :label="$t('UserProfile.username')"
                     disabled
                     id="profile-username"
                     aria-disabled="true"
@@ -24,11 +26,13 @@
                 />
               </v-flex>
             </v-layout>
-            <v-layout row wrap>
-              <v-flex xs12 md12>
+            <v-layout row align-center wrap>
+              <v-flex xs3>
+                <span>{{ $t('UserProfile.administrator') }}</span>
+              </v-flex>
+              <v-flex xs9>
                 <v-checkbox
                     v-model="user.admin"
-                    :label="$t('UserProfile.administrator')"
                     disabled
                     id="profile-admin"
                     aria-disabled="true"
@@ -36,15 +40,14 @@
                 />
               </v-flex>
             </v-layout>
-            <v-layout row wrap>
-              <v-flex
-                  xs12
-                  md12
-              >
+            <v-layout row align-center wrap>
+              <v-flex xs3>
+                <span>{{ $t('UserProfile.groups') }}</span>
+              </v-flex>
+              <v-flex xs9>
                 <v-select
                     :items="user.groups"
                     v-model="user.groups"
-                    :label="$t('UserProfile.groups')"
                     attach
                     multiple
                     disabled
@@ -54,19 +57,39 @@
                 />
               </v-flex>
             </v-layout>
-            <v-layout row wrap>
-              <v-flex
-                  xs12
-                  md12
-              >
+            <v-layout row align-center wrap>
+              <v-flex xs3>
+                <span>{{ $t('UserProfile.created') }}</span>
+              </v-flex>
+              <v-flex xs9>
                 <v-text-field
                     :value="user.created"
-                    :label="$t('UserProfile.created')"
                     disabled
                     id="profile-created"
                     aria-disabled="true"
                     class="body-1"
                 />
+              </v-flex>
+            </v-layout>
+            <v-row mt-4>
+              <v-flex xs12>
+                <p class="title">Preferences</p>
+              </v-flex>
+            </v-row>
+            <v-layout row align-center wrap>
+              <v-flex xs3>
+                <span>Font size</span>
+              </v-flex>
+              <v-flex xs9>
+                <v-btn depressed class="mr-2">
+                  Reset
+                </v-btn>
+                <v-btn depressed class="mx-2">
+                  <v-icon>mdi-format-font-size-decrease</v-icon>
+                </v-btn>
+                <v-btn depressed class="ml-2">
+                  <v-icon>mdi-format-font-size-increase</v-icon>
+                </v-btn>
               </v-flex>
             </v-layout>
           </v-container>
