c00d0320af9b636b89a87b56490501089038564b
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-05-19 14:49:19 +1200

    Add e2e tests for Alert component

---
+++ tests/e2e/specs/alert.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright (C) NIWA & British Crown (Met Office) & Contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import Alert from '@/model/Alert.model'
+
+const getStore = () => cy.window().its('app.$store')
+
+const COLOR_BLACK = 'rgb(0, 0, 0)'
+const COLOR_WHITE = 'rgb(255, 255, 255)'
+
+describe('Alert component', () => {
+  it('Is displayed when an alert is present in the central data store', () => {
+    cy.visit('/#/')
+    const errorMessage = 'Error displayed'
+    getStore().then(store => {
+      store.dispatch('setAlert', new Alert(errorMessage, null, 'error'))
+    })
+    cy
+      .get('.v-alert')
+      .contains(errorMessage)
+  })
+  it('Uses the right color for success', () => {
+    cy.visit('/#/')
+    getStore().then(store => {
+      cy.wrap(store).invoke('commit', 'SET_ALERT', new Alert('An alert', null, 'success'))
+      cy.get('.v-alert').then($alertElements => {
+        const backgroundColor = window.getComputedStyle($alertElements[0])['background-color']
+        expect(backgroundColor).to.not.equal(COLOR_BLACK)
+        expect(backgroundColor).to.not.equal(COLOR_WHITE)
+      })
+      store.commit('SET_ALERT', null)
+    })
+  })
+  it('Uses the right color for warning', () => {
+    cy.visit('/#/')
+    getStore().then(store => {
+      cy.wrap(store).invoke('commit', 'SET_ALERT', new Alert('An alert', null, 'warning'))
+      cy.get('.v-alert').then($alertElements => {
+        const backgroundColor = window.getComputedStyle($alertElements[0])['background-color']
+        expect(backgroundColor).to.not.equal(COLOR_BLACK)
+        expect(backgroundColor).to.not.equal(COLOR_WHITE)
+      })
+      store.commit('SET_ALERT', null)
+    })
+  })
+  it('Uses the right color for error', () => {
+    cy.visit('/#/')
+    getStore().then(store => {
+      cy.wrap(store).invoke('commit', 'SET_ALERT', new Alert('An alert', null, 'error'))
+      cy.get('.v-alert').then($alertElements => {
+        const backgroundColor = window.getComputedStyle($alertElements[0])['background-color']
+        expect(backgroundColor).to.not.equal(COLOR_BLACK)
+        expect(backgroundColor).to.not.equal(COLOR_WHITE)
+      })
+      store.commit('SET_ALERT', null)
+    })
+  })
+})
