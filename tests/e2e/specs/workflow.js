1821a76ef84afb9467599879d81933a4e2e58ec0
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-05-29 12:42:45 +1200

    Add tests for the Workflow view and component/widget

---
+++ tests/e2e/specs/workflow.js
@@ -0,0 +1,98 @@
+/**
+ * Copyright (C) NIWA & British Crown (Met Office) & Contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+describe('Workflow view and component/widget', () => {
+  it('Should display the Workflow component in the Workflow view, with a Tree widget', () => {
+    cy.visit('/#/workflows/one')
+    cy
+      .get('.lm-TabBar-tabLabel')
+      .should('have.length', 1)
+  })
+  it('Should remove the default widget and leave no more widgets', () => {
+    cy.visit('/#/workflows/one')
+    cy
+      .get('.lm-TabBar-tabCloseIcon')
+      .click()
+    cy
+      .get('.lm-TabBar-tabLabel')
+      .should('not.exist')
+  })
+  it('Should be able to add two widgets of the same type', () => {
+    cy.visit('/#/workflows/one')
+    cy
+      .get('.lm-TabBar-tabLabel')
+      .should('have.length', 1)
+    cy
+      .get('a.add-view')
+      .click()
+    cy
+      .get('#toolbar-add-tree-view')
+      .click()
+    cy
+      .get('.lm-TabBar-tabLabel')
+      .should('have.length', 2)
+  })
+  it('Should be able to add three widgets of different types', () => {
+    cy.visit('/#/workflows/one')
+    cy
+      .get('.lm-TabBar-tabLabel')
+      .should('have.length', 1)
+    // add a graph view
+    cy
+      .get('a.add-view')
+      .click()
+    cy
+      .get('#toolbar-add-graph-view')
+      .click()
+    // add a mutations view
+    cy
+      .get('a.add-view')
+      .click()
+    cy
+      .get('#toolbar-add-mutations-view')
+      .click()
+    // ensure we have 3 widgets now
+    cy
+      .get('.lm-TabBar-tabLabel')
+      .should('have.length', 3)
+  })
+  it('Should remove widgets added successfully', () => {
+    cy.visit('/#/workflows/one')
+    cy
+      .get('.lm-TabBar-tabLabel')
+      .should('have.length', 1)
+    // add a graph view
+    cy
+      .get('a.add-view')
+      .click()
+    cy
+      .get('#toolbar-add-graph-view')
+      .click()
+    // ensure we have 2 widgets now
+    cy
+      .get('.lm-TabBar-tabLabel')
+      .should('have.length', 2)
+    // close all widgets
+    cy
+      .get('.lm-TabBar-tabCloseIcon')
+      .click({ multiple: true })
+    // ensure we have no widgets now
+    cy
+      .get('.lm-TabBar-tabLabel')
+      .should('not.exist')
+  })
+})
