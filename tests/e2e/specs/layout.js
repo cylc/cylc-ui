de754e260617669b3d9f8086a616ffb977fe1bd7
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-09-17 15:03:11 +1200

    Add an e2e test for the layout errorCaptured function

---
+++ tests/e2e/specs/layout.js
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C) NIWA & British Crown (Met Office) & Contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+describe('Default layout', () => {
+  it('Should display errors from children elements captured at the Default layout level', () => {
+    // visit any page first, so that we create the window.app reference
+    cy.visit('/#/workflows/one')
+    cy
+      .get('.v-alert')
+      .should('not.be.visible')
+    cy.window().its('app.$workflowService').then(service => {
+      // mock service so that it returns an error
+      cy.stub(service, 'subscribe', () => {
+        throw new Error('Error raised in Cypress stub!')
+      })
+      // now visit dashboard, that calls service.subscribe, which will raise an uncaught error...
+      cy.visit('/#/')
+      cy
+        .get('.v-alert')
+        .should('be.visible')
+    })
+  })
+})
