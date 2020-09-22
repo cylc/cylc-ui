10421dfcbd58dd93261cd65f369ecc2a374512af
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-09-22 15:30:37 +1200

    Add workflow and task states filters to GScan

--- src/components/cylc/gscan/GScan.vue
+++ src/components/cylc/gscan/GScan.vue
@@ -36,20 +36,64 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           placeholder="Search"
           class="flex-grow-1 flex-column"
         />
-        <v-btn
-          link
-          icon
-          class="flex-grow-0 flex-column"
+        <v-menu
+          v-model="showFilterTooltip"
+          :close-on-content-click="false"
+          offset-x
         >
-          <v-icon>mdi-filter</v-icon>
-        </v-btn>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              v-bind="attrs"
+              v-on="on"
+              link
+              icon
+              class="flex-grow-0 flex-column"
+              @click="showFilterTooltip = !showFilterTooltip"
+            >
+              <v-icon>mdi-filter</v-icon>
+            </v-btn>
+          </template>
+          <v-card
+            max-height="250px"
+          >
+            <v-list
+              dense
+            >
+              <div
+                v-for="filter in filters"
+                :key="filter.title"
+              >
+                <v-list-item dense>
+                  <v-list-item-content ma-0>
+                    <v-list-item-title>{{ filter.title }}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+                <v-divider />
+                <v-list-item
+                  v-for="item in filter.items"
+                  :key="`${filter.title}-${item.text}`"
+                  dense
+                >
+                  <v-list-item-action>
+                    <v-checkbox
+                      :label="item.text"
+                      v-model="item.model"
+                      dense
+                      height="20"
+                    ></v-checkbox>
+                  </v-list-item-action>
+                </v-list-item>
+              </div>
+            </v-list>
+          </v-card>
+        </v-menu>
       </div>
       <div
-        v-if="!isLoading && workflows && workflows.length > 0"
+        v-if="!isLoading && filteredWorkflows && filteredWorkflows.length > 0"
         class="c-gscan-workflows"
       >
         <div
-          v-for="workflow in sortedWorkflows"
+          v-for="workflow in filteredWorkflows"
           :key="workflow.id"
           class="c-gscan-workflow"
         >
@@ -129,6 +173,7 @@ import Job from '@/components/cylc/Job'
 import { getWorkflowSummary } from '@/components/cylc/gscan/index'
 import { GSCAN_QUERY } from '@/graphql/queries'
 import WorkflowState from '@/model/WorkflowState.model'
+import TaskState from '@/model/TaskState.model'
 
 const QUERIES = {
   root: GSCAN_QUERY
@@ -155,7 +200,94 @@ export default {
       subscriptions: {},
       isLoading: true,
       maximumTasksDisplayed: 5,
-      searchWorkflows: ''
+      /**
+       * The filtered workflows. This is the result of applying the filters
+       * on the workflows prop.
+       * @type {[
+       *   {
+       *     id: string,
+       *     name: string,
+       *     stateTotals: object,
+       *     status: string
+       *   }
+       * ]}
+       */
+      filteredWorkflows: [],
+      /**
+       * Value to search and filter workflows.
+       * @type {string}
+       */
+      searchWorkflows: '',
+      /**
+       * Variable to control whether the filters tooltip (a menu actually)
+       * is displayed or not (i.e. v-model=this).
+       * @type {boolean}
+       */
+      showFilterTooltip: false,
+      /**
+       * List of filters to be displayed by the template, so that the user
+       * can filter the list of workflows.
+       *
+       * Each entry contains a title and a list of items. Each item contains
+       * the text attribute, which is used as display value in the template.
+       * The value attribute, which may be used if necessary, as it contains
+       * the original value (e.g. an Enum, while title would be some formatted
+       * string). Finally, the model is bound via v-model, and keeps the
+       * value selected in the UI (i.e. if the user checks the "running"
+       * checkbox, the model here will be true, if the user unchecks it,
+       * then it will be false).
+       *
+       * @type {[
+       *   {
+       *     title: string,
+       *     items: [{
+       *       text: string,
+       *       value: object,
+       *       model: boolean
+       *     }]
+       *   }
+       * ]}
+       */
+      filters: [
+        {
+          title: 'workflow state',
+          items: [
+            {
+              text: 'running',
+              value: 'running',
+              model: true
+            },
+            {
+              text: 'held',
+              value: 'held',
+              model: true
+            },
+            {
+              text: 'stopped',
+              value: 'stopped',
+              model: true
+            }
+          ]
+        },
+        {
+          title: 'task state',
+          items: TaskState.enumValues.map(state => {
+            return {
+              text: state.name.toLowerCase(),
+              value: state,
+              model: true
+            }
+          })
+        }
+        // {
+        //   title: 'workflow host',
+        //   items: [] // TODO: will it be in state totals?
+        // },
+        // {
+        //   title: 'cylc version',
+        //   items: [] // TODO: will it be in state totals?
+        // }
+      ]
     }
   },
   computed: {
@@ -195,13 +327,40 @@ export default {
         }
       }
       return workflowSummaries
-    },
-    filteredWorkflows () {
-      let filtered = [...this.workflows]
-      if (this.searchWorkflows && this.searchWorkflows !== '') {
-        filtered = filtered.filter(workflow => workflow.name.includes(this.searchWorkflows))
+    }
+  },
+  watch: {
+    /**
+     * If the user changes the list of filters, then we apply
+     * the filters to the list of workflows.
+     */
+    filters: {
+      deep: true,
+      immediate: false,
+      handler: function (newVal, _) {
+        this.filterWorkflows(this.workflows, this.searchWorkflows, newVal)
       }
-      return filtered
+    },
+    /**
+     * If the user changes the workflow name to search/filter,
+     * then we apply the filters to the list of workflows.
+     */
+    searchWorkflows: {
+      immediate: false,
+      handler: function (newVal, _) {
+        this.filterWorkflows(this.workflows, newVal, this.filters)
+      }
+    },
+    /**
+     * If the subscription changes the workflows object (any part of it),
+     * then we apply the filters to the list of workflows.
+     */
+    workflows: {
+      deep: true,
+      immediate: true,
+      handler: function (newVal, _) {
+        this.filterWorkflows(newVal, this.searchWorkflows, this.filters)
+      }
     }
   },
   created () {
@@ -263,6 +422,62 @@ export default {
       return {
         'c-workflow-stopped': status === WorkflowState.STOPPED.name
       }
+    },
+
+    /**
+     * Filter a list of workflows using a given name (could be a part of
+     * a name) and a given list of filters.
+     *
+     * The list of filters may contain workflow states ("running", "stopped",
+     * "held"), and/or task states ("running", "waiting", "submit_failed", etc).
+     *
+     * Does not return any value, but modifies the data variable
+     * filteredWorkflows, used in the template.
+     *
+     * @param {[
+     *  {
+     *   id: string,
+     *   name: string,
+     *   stateTotals: object,
+     *   status: string
+     *  }
+     * ]} workflows list of workflows
+     * @param {string} name
+     * @param {[]} filters
+     */
+    filterWorkflows (workflows, name, filters) {
+      // filter by name
+      this.filteredWorkflows = workflows
+      if (name && name !== '') {
+        this.filteredWorkflows = this.filteredWorkflows
+          .filter(workflow => workflow.name.includes(name))
+      }
+      // get a list of the workflow states we are filtering
+      const workflowStates = filters[0]
+        .items
+        .filter(item => item.model)
+        .map(item => item.value)
+      // get a list of the task states we are filtering
+      const taskStates = new Set(filters[1]
+        .items
+        .filter(item => item.model)
+        .map(item => item.value.name.toLowerCase()))
+      // filter workflows
+      this.filteredWorkflows = this.filteredWorkflows.filter((workflow) => {
+        // workflow states
+        if (!workflowStates.includes(workflow.status)) {
+          return false
+        }
+        // task states (but only if non-stopped, as otherwise stateTotals will be empty)
+        if (workflow.status === 'running') {
+          const thisWorkflowStates = Object.entries(workflow.stateTotals)
+            .filter(entry => entry[1] > 0)
+            .map(entry => entry[0])
+          const intersection = thisWorkflowStates.filter(item => taskStates.has(item))
+          return intersection.length !== 0
+        }
+        return true
+      })
     }
   }
 }
