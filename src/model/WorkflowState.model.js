282deca068ba8a8dbd305a4a4cf69802bd50b6dd
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2020-10-21 11:33:50 +1300

    Add a WorkflowState enum

---
+++ src/model/WorkflowState.model.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright (C) NIWA & British Crown (Met Office) & Contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { Enumify } from 'enumify'
+
+/**
+ * Cylc valid workflow states.
+ */
+class WorkflowState extends Enumify {
+  static RUNNING = new WorkflowState('running')
+  static HELD = new WorkflowState('held')
+  static STOPPING = new WorkflowState('stopping')
+  static STOPPED = new WorkflowState('stopped')
+  static ERROR = new WorkflowState('error')
+  static _ = this.closeEnum()
+
+  /**
+   * Constructor.
+   * @param {String} name
+   */
+  constructor (name) {
+    super()
+    this.name = name
+  }
+}
+
+export default WorkflowState
