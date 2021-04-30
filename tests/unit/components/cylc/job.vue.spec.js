ecf2a9b5b9210082955991942e18f431b90318ff
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2021-04-30 12:56:32 +1200

    Add a unit test for the Job component, including the new multiple prop

---
+++ tests/unit/components/cylc/job.vue.spec.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) NIWA & British Crown (Met Office) & Contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { mount } from '@vue/test-utils'
+import { expect } from 'chai'
+// import vuetify here so that we do not have warnings in the console output
+// eslint-disable-next-line no-unused-vars
+import * as vuetify from '@/plugins/vuetify'
+import Job from '@/components/cylc/Job'
+
+describe('Job component', () => {
+  it('should initialize props', () => {
+    const wrapper = mount(Job, {
+      context: {
+        props: {
+          status: '',
+          multiple: false
+        }
+      }
+    })
+    expect(wrapper.element.className).to.equal('c-job')
+    expect(wrapper.get('.job').element.childElementCount).to.equal(1)
+  })
+  it('should add a new class for multiple jobs', () => {
+    const wrapper = mount(Job, {
+      context: {
+        props: {
+          status: '',
+          multiple: true
+        }
+      }
+    })
+    // The shadow is added as an extra child element for the Job SVG (two rects).
+    expect(wrapper.get('.job').element.childElementCount).to.equal(2)
+  })
+})
