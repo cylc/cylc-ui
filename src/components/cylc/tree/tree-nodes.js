4fea416ebaa46658f8a8694dda01865a10a0e624
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2021-05-03 09:18:26 +1200

    Add .model suffix just to keep things uniform

--- src/components/cylc/tree/tree-nodes.js
+++ src/components/cylc/tree/tree-nodes.js
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import TaskOutput from '@/model/TaskOutput'
+import TaskOutput from '@/model/TaskOutput.model'
 import Vue from 'vue'
 
 /**
