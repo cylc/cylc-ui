4218e87534e018dc779ba06c56910c55a71d968c
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2021-05-21 12:29:19 +1200

    Add initial test for the Table component

---
+++ tests/unit/components/cylc/table/table.data.js
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) NIWA & British Crown (Met Office) & Contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import TaskState from '@/model/TaskState.model'
+import JobState from '@/model/JobState.model'
+
+const BASE_ID = 'cylc|workflow|1'
+
+const simpleTableTasks = [
+  {
+    id: `${BASE_ID}|taskA`,
+    state: TaskState.RUNNING.name,
+    name: 'taskA',
+    meanElapsedTime: 2000,
+    jobs: [
+      {
+        platform: 'localhost',
+        jobRunnerName: 'background',
+        jobId: '1',
+        submittedTime: new Date(),
+        startedTime: new Date(),
+        finishedTime: null,
+        state: JobState.RUNNING.name
+      }
+    ]
+  },
+  {
+    id: `${BASE_ID}|taskB`,
+    state: TaskState.WAITING.name,
+    name: 'taskB',
+    jobs: []
+  },
+  {
+    id: `${BASE_ID}|taskB`,
+    state: TaskState.SUBMITTED.name,
+    name: 'taskB',
+    jobs: []
+  }
+]
+
+export {
+  simpleTableTasks
+}
---
+++ tests/unit/components/cylc/table/table.vue.spec.js
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C) NIWA & British Crown (Met Office) & Contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { createLocalVue, mount } from '@vue/test-utils'
+import { expect } from 'chai'
+import CylcObjectPlugin from '@/components/cylc/cylcObject/plugin'
+import Table from '@/components/cylc/table/Table'
+import Vuetify from 'vuetify/lib'
+import { simpleTableTasks } from './table.data'
+
+const localVue = createLocalVue()
+localVue.prototype.$eventBus = {
+  emit: () => {}
+}
+localVue.prototype.$workflowService = {
+  register: function () {
+  },
+  unregister: function () {
+  },
+  subscribe: function () {
+  },
+  mutations: [
+    {
+      args: []
+    }
+  ]
+}
+localVue.use(CylcObjectPlugin)
+
+describe('Table component', () => {
+  let vuetify
+  beforeEach(() => {
+    vuetify = new Vuetify({
+      theme: { disable: true },
+      icons: {
+        iconfont: 'mdi'
+      }
+    })
+  })
+  // mount function from Vuetify docs https://vuetifyjs.com/ru/getting-started/unit-testing/
+  /**
+   * @param options
+   * @returns {Wrapper<Tree>}
+   */
+  const mountFunction = options => {
+    // the mocks.$vuetify is for https://github.com/vuetifyjs/vuetify/issues/9923
+    return mount(Table, {
+      localVue,
+      mocks: {
+        $vuetify: {
+          lang: {
+            t: (val) => val
+          }
+        }
+      },
+      vuetify,
+      ...options
+    })
+  }
+  global.requestAnimationFrame = cb => cb()
+  it('should display the table with valid data', () => {
+    const wrapper = mountFunction({
+      propsData: {
+        tasks: simpleTableTasks
+      }
+    })
+    expect(wrapper.props().tasks[0].name).to.equal('taskA')
+    expect(wrapper.find('div')).to.not.equal(null)
+  })
+})
