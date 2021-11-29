8eb229805cf1e2110d01db20f4c8119e93b0caca
Aaron Cole <aaron@epiphron.co.nz>
2021-11-29 18:53:03 +1300

    Fixed tests

--- tests/e2e/specs/table.js
+++ tests/e2e/specs/table.js
@@ -57,9 +57,6 @@ describe('Table view', () => {
         .get('#c-table-filter-task-name')
         .type('eep')
       cy
-        .get('#c-table-filter-btn')
-        .click()
-      cy
         .get('td > div.d-flex > div')
         .contains('sleepy')
         .should('be.visible')
@@ -86,9 +83,6 @@ describe('Table view', () => {
         .contains(TaskState.RUNNING.name)
         .click({ force: true })
       cy
-        .get('#c-table-filter-btn')
-        .click({ force: true })
-      cy
         .get('td > div.d-flex > div')
         .contains('checkpoint')
         .should('be.visible')
@@ -111,18 +105,12 @@ describe('Table view', () => {
         .contains(TaskState.SUBMITTED.name)
         .click({ force: true })
       cy
-        .get('#c-table-filter-btn')
-        .click({ force: true })
-      cy
         .get('.c-table table > tbody > tr')
         .should('have.length', 3)
         .should('be.visible')
       cy
         .get('#c-table-filter-task-name')
         .type(TaskState.FAILED.name)
-      cy
-        .get('#c-table-filter-btn')
-        .click()
       cy
         .get('td > div.d-flex > div')
         .contains(TaskState.FAILED.name)
