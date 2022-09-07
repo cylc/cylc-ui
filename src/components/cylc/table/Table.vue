de04debf5ca0f22c6f0e0e7d74b0c52c03c20ef3
Aaron Cole <aaron.cole@niwa.co.nz>
2022-09-07 16:37:47 +1200

    Added a check to see what user preference was for cycle point ordering

--- src/components/cylc/table/Table.vue
+++ src/components/cylc/table/Table.vue
@@ -99,6 +99,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           fluid
           class="ma-0 pa-0 w-100 h-100 left-0 top-0 position-absolute"
         >
+          {{  }}
           <v-data-table
             :headers.sync="headers"
             :items.sync="filteredTasks"
@@ -241,7 +242,7 @@ export default {
         mdiArrowDown
       },
       sortBy: ['Cycle Point'],
-      sortDesc: [true],
+      sortDesc: [localStorage.cyclePointsOrderDesc ? JSON.parse(localStorage.cyclePointsOrderDesc) : true],
       expanded: [],
       headers: [
         {
