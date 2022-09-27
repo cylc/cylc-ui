9edf7fe0bbec9fa04c4f3618b99e7caabb4a4311
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2022-09-27 13:46:26 +0100

    graph node: contstrain task node to 100x100 box

--- src/components/cylc/GraphNode.vue
+++ src/components/cylc/GraphNode.vue
@@ -6,11 +6,22 @@
       :isRunahead="task.node.isRunahead"
       :isQueued
     -->
-    <task
-      :svg="true"
-      :status="task.node.state"
-      v-cylc-object="task.node"
-    />
+    <symbol :id="nodeID" viewBox="0 0 100 100">
+      <!--
+        Use a "symbol" for the task node in order to apply a viewBox to it.
+        This is to prevent the task icon from overflowing the 100x100 box it
+        is supposed to be contained in. Due to the way the progress icon
+        works the BBox of a task icon is greater than this 100x100 box for
+        running tasks.
+      -->
+      <task
+        :svg="true"
+        :status="task.node.state"
+        v-cylc-object="task.node"
+      />
+    </symbol>
+    <use :href="`#${nodeID}`" x="0" y="0" width="100" height="100" />
+
     <text
       x="120" y="35"
       font-size="50"
@@ -59,6 +70,11 @@ export default {
   props: {
     task: {
       required: true
+    }
+  },
+  computed: {
+    nodeID () {
+      return `graph-node-${this.task.id}`
     }
   }
 }
