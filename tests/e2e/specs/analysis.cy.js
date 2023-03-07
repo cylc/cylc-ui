4d949ced7e8ffbca1e67140981ac251a9d939101
JAllen42 <jrallen42@gmail.com>
2023-03-07 10:42:22 +0000

    Extend end-to-end tests

--- tests/e2e/specs/analysis.cy.js
+++ tests/e2e/specs/analysis.cy.js
@@ -46,5 +46,167 @@ describe('Analysis view', () => {
         .find('input[type="hidden"]')
         .should('have.value', 'totalTimes')
     })
+    it('Should filter by task name', () => {
+      cy.visit('/#/analysis/one')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 3)
+        .should('be.visible')
+      cy
+        .get('td')
+        .contains('eventually')
+        .should('be.visible')
+      cy
+        .get('#c-analysis-filter-task-name')
+        .type('wait')
+      cy
+        .get('td')
+        .contains('waiting')
+        .should('be.visible')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 1)
+        .should('be.visible')
+    })
+    it('Should filter by task platform', () => {
+      cy.visit('/#/analysis/one')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 3)
+        .should('be.visible')
+      cy
+        .get('td')
+        .contains('platform_1')
+        .should('be.visible')
+      cy
+        .get('#c-analysis-filter-task-platforms')
+        .click({ force: true })
+      cy
+        .get('.v-list-item')
+        .contains('platform_2')
+        .click({ force: true })
+      cy
+        .get('td')
+        .contains('eventually')
+        .should('be.visible')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 1)
+        .should('be.visible')
+    })
+    it('Should display the correct timings', () => {
+      cy.visit('/#/analysis/one')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 3)
+        .should('be.visible')
+      cy
+        .get('td')
+        .contains('30')
+        .should('be.visible')
+      // Show run times
+      cy
+        .get('#c-analysis-filter-task-timings')
+        .click({ force: true })
+      cy
+        .get('.v-list-item')
+        .contains('Run')
+        .click({ force: true })
+      cy
+        .get('td')
+        .contains('21')
+        .should('be.visible')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 3)
+        .should('be.visible')
+      // Show queue times
+      cy
+        .get('#c-analysis-filter-task-timings')
+        .click({ force: true })
+      cy
+        .get('.v-list-item')
+        .contains('Queue')
+        .click({ force: true })
+      cy
+        .get('td')
+        .contains('12')
+        .should('be.visible')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 3)
+        .should('be.visible')
+    })
+    it('Should filter by task name, platform and timings', () => {
+      cy.visit('/#/analysis/one')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 3)
+        .should('be.visible')
+      cy
+        .get('td')
+        .contains('30')
+        .should('be.visible')
+      // Show only task on platform_1
+      cy
+        .get('#c-analysis-filter-task-platforms')
+        .click({ force: true })
+      cy
+        .get('.v-list-item')
+        .contains('platform_1')
+        .click({ force: true })
+      cy
+        .get('td')
+        .contains('waiting')
+        .should('be.visible')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 2)
+        .should('be.visible')
+      // Show run times
+      cy
+        .get('#c-analysis-filter-task-timings')
+        .click({ force: true })
+      cy
+        .get('.v-list-item')
+        .contains('Run')
+        .click({ force: true })
+      cy
+        .get('td')
+        .contains('21')
+        .should('be.visible')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 2)
+        .should('be.visible')
+      // Show task names containing 'wait'
+      cy
+        .get('#c-analysis-filter-task-name')
+        .type('wait')
+      cy
+        .get('td')
+        .contains('waiting')
+        .should('be.visible')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 1)
+        .should('be.visible')
+      // Show queue times
+      cy
+        .get('#c-analysis-filter-task-timings')
+        .click({ force: true })
+      cy
+        .get('.v-list-item')
+        .contains('Queue')
+        .click({ force: true })
+      cy
+        .get('td')
+        .contains('12')
+        .should('be.visible')
+      cy
+        .get('.c-analysis table > tbody > tr')
+        .should('have.length', 1)
+        .should('be.visible')
+    })
   })
 })
