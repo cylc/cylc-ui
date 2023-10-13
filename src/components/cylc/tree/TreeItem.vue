c2ecffd693d7d89cecdc4745b4fd26154ebf9784
Oliver Sanders <oliver.sanders@metoffice.gov.uk>
2023-10-13 13:12:09 +0100

    fix a couple of possible errors
    * Fixes an issue where a task might not have a firstParent.
      * Closes #1470
    * Fixes a traceback when opening the tree view.
      * To replicate, close all views, open the simple tree view,
        wait for it to load, then open the tree view.

--- src/components/cylc/tree/TreeItem.vue
+++ src/components/cylc/tree/TreeItem.vue
@@ -48,7 +48,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
             type to family. -->
           <Task
             v-cylc-object="node"
-            v-if="node.familyTree"
+            v-if="node.familyTree?.length"
             :key="node.id"
             :task="node.familyTree[0].node"
           />
