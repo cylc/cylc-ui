6a3cda6427925a9a85fed31dcedf5d21efd533a8
Mark Dawson <mark.dawson@metoffice.gov.uk>
2023-10-25 13:19:51 +0100

    Ability to store & remove server owner & deployment entries in dropdowns (#1503)

---
+++ tests/unit/composables/useLocalStorage.spec.js
@@ -0,0 +1,26 @@
+import useLocalStorage from '@/composables/useLocalStorage.js'
+
+describe('useLocalStorage composable', () => {
+  const {
+    items,
+    addToLocalStorage,
+    removeFromLocalStorage
+  } = useLocalStorage('itemsLocalStoreKey', 'test initial item')
+
+  localStorage.setItem('itemsLocalStoreKey', JSON.stringify(Array.from(items.value)))
+
+  it('should add a value to the local store', () => {
+    addToLocalStorage('test item')
+    expect(Array.from(items.value)).toStrictEqual(['test initial item', 'test item'])
+  })
+
+  it('should not add more than one of the same value to the local store', () => {
+    addToLocalStorage('test item')
+    expect(Array.from(items.value)).toStrictEqual(['test initial item', 'test item'])
+  })
+
+  it('should remove a value to the local store', () => {
+    removeFromLocalStorage('test item')
+    expect(Array.from(items.value)).toStrictEqual(['test initial item'])
+  })
+})
