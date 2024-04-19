7c732f4bbddde09316747865682e0b951d691f4d
Mark Dawson <mark.dawson@metoffice.gov.uk>
2024-04-19 16:07:08 +0100

    added cyclepoint grouping with subgraphs
    added toggle button and styling
    
    added e2e test
    
    use initialOptions prop to save view state
    
    fix nodes undefined error
    
    added towncrier entry
    
    review amends
    
    review amends
    
    fix broken e2e tests

---
+++ src/components/cylc/GraphSubgraph.vue
@@ -0,0 +1,48 @@
+<template>
+  <g class="c-graph-subgraph">
+    <rect
+      :width="subgraph.width"
+      :height="subgraph.height"
+      :x="subgraph.x"
+      :y="subgraph.y"
+      rx="50"
+      ry="50"
+      fill="none"
+      stroke-width="8px"
+      stroke="grey"
+      stroke-dasharray="50 50"
+      />
+    <text
+      :x="labelXPosition"
+      :y="labelYPosition"
+      font-family="Roboto"
+      alignment-baseline="middle" text-anchor="middle"
+      font-size="70px"
+      fill="black"
+      stroke-width=1.5
+      stroke="white"
+    >
+      {{ subgraph.label }}
+    </text>
+  </g>
+</template>
+
+<script>
+export default {
+  name: 'GraphSubgraph',
+  props: {
+    subgraph: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    labelXPosition () {
+      return (parseInt(this.subgraph.x) + (parseInt(this.subgraph.width) / 2))
+    },
+    labelYPosition () {
+      return (parseInt(this.subgraph.y) + 90)
+    },
+  }
+}
+</script>
