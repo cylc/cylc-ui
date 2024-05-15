6abbdec6f55fd5c9d48d1fb63ca9ff064b24d6fe
Mark Dawson <mark.dawson@metoffice.gov.uk>
2024-05-15 16:01:06 +0100

    Added comments and copyright notice

--- src/components/cylc/GraphSubgraph.vue
+++ src/components/cylc/GraphSubgraph.vue
@@ -1,3 +1,23 @@
+<!--
+Copyright (C) NIWA & British Crown (Met Office) & Contributors.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-->
+
+<!--
+  Subgraph - Used to render graphviz subgraphs as svg.
+-->
 <template>
   <g class="c-graph-subgraph">
     <rect
@@ -42,6 +62,9 @@ export default {
       return (parseInt(this.subgraph.x) + (parseInt(this.subgraph.width) / 2))
     },
     labelYPosition () {
+      // Graphviz puts labels inside the subgraph
+      // SVG rect text is put outside the rect
+      // Adding 90pt to the y position brings the label inside the rect
       return (parseInt(this.subgraph.y) + 90)
     },
   }
