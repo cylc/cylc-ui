0b534daae9ab388b3170cf396fb23caf25a91c32
Mark Dawson <mark.dawson@metoffice.gov.uk>
2024-07-10 15:38:52 +0100

    converted component to script setup

--- src/components/cylc/Drawer.vue
+++ src/components/cylc/Drawer.vue
@@ -27,13 +27,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
       <v-list
         class="pa-0 d-flex flex-column"
       >
-        <c-header />
+        <CHeader />
 
         <v-list-item
           to="/"
         >
           <template v-slot:prepend>
-            <v-icon style="opacity: 1;">{{ $options.icons.mdiHome }}</v-icon>
+            <v-icon style="opacity: 1;">{{ icons.mdiHome }}</v-icon>
           </template>
           <v-list-item-title>Dashboard</v-list-item-title>
         </v-list-item>
@@ -43,7 +43,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
           class="v-list-item"
         >
           <template v-slot:prepend>
-            <v-icon style="opacity: 1;">{{ $options.icons.mdiGraphql }}</v-icon>
+            <v-icon style="opacity: 1;">{{ icons.mdiGraphql }}</v-icon>
           </template>
           <v-list-item-title>GraphiQL</v-list-item-title>
         </v-list-item>
@@ -59,89 +59,70 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     <template v-slot:append>
       <div class="px-4 py-2 d-flex justify-center">
         <span class="text--secondary">
-          <strong v-if="$options.mode !== 'production'">{{ $options.mode.toUpperCase() }}</strong> {{ $t('App.name') }} {{ $options.version }}
+          <strong v-if="mode !== 'production'">{{ mode.toUpperCase() }}</strong> {{ $t('App.name') }} {{ pkg.version }}
         </span>
       </div>
     </template>
   </v-navigation-drawer>
 </template>
 
-<script>
+<script setup>
 import { nextTick, ref } from 'vue'
 import { useDisplay } from 'vuetify'
-import Header from '@/components/cylc/Header.vue'
+import CHeader from '@/components/cylc/Header.vue'
 import Workflows from '@/views/Workflows.vue'
 import { mdiHome, mdiGraphql } from '@mdi/js'
 import pkg from '@/../package.json'
 import { when } from '@/utils'
 import { useDrawer } from '@/utils/toolbar'
 
-export const initialWidth = 260
-export const minWidth = 150
+const { mobile } = useDisplay()
 
-export default {
-  components: {
-    Workflows,
-    'c-header': Header
-  },
+const initialWidth = 260
+const minWidth = 150
 
-  setup () {
-    const { mobile } = useDisplay()
+const drawerWidth = ref(initialWidth)
 
-    const drawerWidth = ref(initialWidth)
+const { drawer } = useDrawer()
+// Show drawer initially if viewport is large enough:
+drawer.value = !mobile.value
 
-    const { drawer } = useDrawer()
-    // Show drawer initially if viewport is large enough:
-    drawer.value = !mobile.value
+const icons = {mdiHome, mdiGraphql}
+const mode = import.meta.env.MODE
 
-    function resize (e) {
-      // If less than min width, will collapse (to 4px because that's the resize-bar width)
-      drawerWidth.value = e.clientX > minWidth ? e.clientX : 4
-    }
-
-    /** @type {import('vue').Ref<HTMLElement>} template ref */
-    const resizeBar = ref(null)
-
-    when(resizeBar, () => {
-      resizeBar.value.addEventListener(
-        'mousedown',
-        (mdEvent) => {
-          document.body.classList.add('resizing-drawer')
-          document.addEventListener('mousemove', resize, { passive: true })
-          mdEvent.stopPropagation?.()
-          mdEvent.preventDefault?.()
-
-          document.addEventListener(
-            'mouseup',
-            (muEvent) => {
-              if (muEvent.clientX < minWidth) {
-                drawer.value = false
-                // Reset to width at time of mousedown
-                nextTick(() => {
-                  drawerWidth.value = mdEvent.clientX
-                })
-              }
-              document.body.classList.remove('resizing-drawer')
-              document.removeEventListener('mousemove', resize)
-            },
-            { once: true }
-          )
-        }
-      )
-    })
-
-    return {
-      drawer,
-      drawerWidth,
-      resizeBar,
-    }
-  },
-
-  icons: {
-    mdiHome,
-    mdiGraphql,
-  },
-  mode: import.meta.env.MODE,
-  version: pkg.version,
+function resize (e) {
+  // If less than min width, will collapse (to 4px because that's the resize-bar width)
+  drawerWidth.value = e.clientX > minWidth ? e.clientX : 4
 }
+
+/** @type {import('vue').Ref<HTMLElement>} template ref */
+const resizeBar = ref(null)
+
+when(resizeBar, () => {
+  resizeBar.value.addEventListener(
+    'mousedown',
+    (mdEvent) => {
+      document.body.classList.add('resizing-drawer')
+      document.addEventListener('mousemove', resize, { passive: true })
+      mdEvent.stopPropagation?.()
+      mdEvent.preventDefault?.()
+
+      document.addEventListener(
+        'mouseup',
+        (muEvent) => {
+          if (muEvent.clientX < minWidth) {
+            drawer.value = false
+            // Reset to width at time of mousedown
+            nextTick(() => {
+              drawerWidth.value = mdEvent.clientX
+            })
+          }
+          document.body.classList.remove('resizing-drawer')
+          document.removeEventListener('mousemove', resize)
+        },
+        { once: true }
+      )
+    }
+  )
+})
 </script>
