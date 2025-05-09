b7bb2b61cbc070a15dd7edae97a08d7ec5c6db88
Mark Dawson <mark.dawson@metoffice.gov.uk>
2025-05-09 11:45:44 +0100

    set default log file by job outcome (#2025)
    set default log file by job outcome
    
    Additionally:
    
    * UID Tokens validates job part and permits `NN`
    * Validate relative ID input in log view
    * Log view: make file input non-clearable - The clear icon takes up space and can cover the filename. I can't think of a reason why a user would want to clear this particular input.
    * Log view: ensure default job log file does not override `initialOptions`
    
    ---------
    
    Co-authored-by: Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>

--- src/services/mock/json/workflows/index.cjs
+++ src/services/mock/json/workflows/index.cjs
@@ -15,29 +15,17 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const IntrospectionQuery = require('./IntrospectionQuery.json')
-const taskProxy = require('./taskProxy.json')
-const familyProxy = require('./familyProxy.json')
-const { one, workflows, Workflow } = require('./workflows/index.cjs')
-const { LogData } = require('./logData.cjs')
-const { LogFiles, JobState } = require('./logFiles.cjs')
-const analysisQuery = require('./analysisQuery.json')
-const ganttQuery = require('./ganttQuery.json')
-const InfoViewSubscription = require('./infoView.json')
+const one = require('./one')
+const multi = require('./multi')
+
+const workflows = [one, ...multi]
+
+function Workflow ({ workflowId }) {
+  return workflows.find(({ deltas }) => deltas.id === workflowId) || {}
+}
 
 module.exports = {
-  IntrospectionQuery,
-  taskProxy,
-  familyProxy,
-  LogData,
-  LogFiles,
-  JobState,
-  App: workflows,
+  one,
+  workflows,
   Workflow,
-  GraphIQLTest: one,
-  analysisTaskQuery: analysisQuery.taskQuery,
-  analysisJobQuery: analysisQuery.jobQuery,
-  analysisQuery,
-  ganttQuery,
-  InfoViewSubscription
 }
