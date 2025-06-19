8206619d0f31c044d6e2e62c929d8359e63381a6
Ronnie Dutta <61982285+MetRonnie@users.noreply.github.com>
2025-06-19 17:00:38 +0100

    Tidy

--- src/components/graphqlFormGenerator/components/vuetify.js
+++ src/components/graphqlFormGenerator/components/vuetify.js
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) NIWA & British Crown (Met Office) & Contributors.
  *
  * This program is free software: you can redistribute it and/or modify
@@ -35,7 +35,7 @@ const NumberFieldProps = {
 }
 
 const RE = {
-  cyclePoint: '\\d+(T\\d+(Z|[+-]\\d+)?)?'
+  cyclePoint: String.raw`\d+(T\d+(Z|[+-]\d+)?)?`,
 }
 
 export const RULES = {
@@ -110,7 +110,7 @@ export default {
       rules: [
         RULES.noSpaces,
         // character whitelist
-        x => Boolean(!x || x.match(`^${RE.cyclePoint}$`)) || 'Invalid Cycle Point'
+        x => Boolean(!x || x.match(String.raw`^${RE.cyclePoint}$`)) || 'Invalid Cycle Point'
       ]
     },
     CyclePointGlob: {
@@ -127,7 +127,7 @@ export default {
     BroadcastCyclePoint: {
       is: VTextField,
       rules: [
-        x => Boolean(!x || x.match(`^(${RE.cyclePoint}|\\*)$`)) || 'Must be "*" or a valid cycle point'
+        x => Boolean(!x || x.match(String.raw`^(${RE.cyclePoint}|\*)$`)) || 'Must be "*" or a valid cycle point'
       ]
     },
     // TaskStatus
